beef735b75b6620bd2baae95f90d7dc21097a414
SEWCHEENDASs <cheendasew150397@gmail.com>
2020-04-23 12:24:30 +0800

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -4,5 +4,25 @@
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 var sum_to_n = function(n) {
-    // your code here
-};
+  let result = 0
+  for (let i = 1; i <= n; i++) {
+    result = result + i
+  }
+  return result
+}
+function stringNumbers(n) {
+  var finalString = ''
+  for (let i = 1; i <= n; i++) {
+    if (i >= n) {
+      finalString += i + ' '
+    } else {
+      finalString += i + ' + '
+    }
+  }
+  return finalString
+}
+
+let n = 5
+console.log(`sum_to_n(${n}) === ${stringNumbers(n)}=== ${sum_to_n(n)}`)
+
+
