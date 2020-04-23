55cffd340682b8fb80331d9a4d4a6a58f92b8f2c
SEWCHEENDASs <cheendasew150397@gmail.com>
2020-04-23 12:35:08 +0800

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,28 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
+let n = 5
+function stringNumbers(n) {
+  var finalString = ''
+  for (let i = 1; i <= n; i++) {
+    if (i >= n) {
+      finalString += i + ' '
+    } else {
+      finalString += i + ' + '
+    }
+  }
+  return finalString
+}
+
 var sum_to_n = function(n) {
-    // your code here
-};
+  let result = 0
+  for (let i = 1; i <= n; i++) {
+    result = result + i
+  }
+  return result
+}
+
+
+console.log(`sum_to_n(${n}) === ${stringNumbers(n)}=== ${sum_to_n(n)}`)
+
+
