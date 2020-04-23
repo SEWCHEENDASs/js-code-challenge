a8acac477fc3d11fc2596bd2e9a722658ac80d26
SEWCHEENDASs <cheendasew150397@gmail.com>
2020-04-23 12:38:28 +0800

    Update solution_a.js
    Hi Sir, 
    I can't seem to indent the code here properly in the above box, so placing the misindented code here
    
    var sum_to_n = function(n) {
      let result = 0
      for (let i = 1; i <= n; i++) {
        result = result + i
      }
      return result
    }

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -2,7 +2,24 @@
 // Task: Provide 3 unique implementations of the following function.
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
-
 var sum_to_n = function(n) {
-    // your code here
-};
+  let result = 0
+  for (let i = 1; i <= n; i++) {
+    result = result + i
+  }
+  return result
+}
+function stringNumbers(n) {
+  var finalString = ''
+  for (let i = 1; i <= n; i++) {
+    if (i >= n) {
+      finalString += i + ' '
+    } else {
+      finalString += i + ' + '
+    }
+  }
+  return finalString
+}
+
+let n = 5
+console.log(`sum_to_n(${n}) === ${stringNumbers(n)}=== ${sum_to_n(n)}`)
